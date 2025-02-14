adaa558ca6487a5b5fdfc8da65db68437c2ebe96
N <Na@>
2025-02-14 10:58:17 +0300

    доп

--- app/src/main/java/com/example/primer/MainActivity.java
+++ app/src/main/java/com/example/primer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.primer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-private Integer count = 0;
+    private Integer count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +25,67 @@ private Integer count = 0;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toast.makeText(this,"onCreate", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Instance", "onStart");
+        Toast.makeText(this,"OnStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Instance", "onStart");
+        Toast.makeText(this,"OnResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("Instance", "onStart");
+        Toast.makeText(this,"OnPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("Instance", "onStart");
+        Toast.makeText(this,"OnStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("Instance", "onStart");
+        Toast.makeText(this,"OnDestroy", Toast.LENGTH_LONG).show();
+    }
+@Override
+    protected void onSaveInstanceState(Bundle autState){
+        super.onSaveInstanceState(autState);
+        autState.putInt("Klic", count);
+        Log.d("Instens","onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle SavedState){
+        super.onRestoreInstanceState(SavedState);
+        if(SavedState != null && SavedState.containsKey("Klic")){
+            count = SavedState.getInt("Klic");
+        }
+        Log.d("Instens","get");
+        Toast.makeText(this, "get",Toast.LENGTH_LONG).show();
     }
     public void onClickBatton(View view) {
         count++;
-        TextView txt= findViewById(R.id.txtCounter);
+        TextView txt = findViewById(R.id.txtCounter);
+        txt.setText(count.toString());
+    }
+
+    public void onClickButton(View view) {
+        count--;
+        TextView txt = findViewById(R.id.txtCounter);
         txt.setText(count.toString());
     }
 }
